ce3f87576e8dc20d47e1f046c08cc0de7bea34e7
LeoCucinell <leocucinell@gmail.com>
2020-12-30 14:08:05 -0800

    initial commit

---
+++ Split/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Split
+//
+//  Created by leo cucinell on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
